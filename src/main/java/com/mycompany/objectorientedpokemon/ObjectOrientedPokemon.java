38843f4c54315a619bbede80d30e3a32fa905413
LUNEi3 <niranon.49@gmail.com>
2025-11-12 09:17:07 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/objectorientedpokemon/ObjectOrientedPokemon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.objectorientedpokemon;
+
+/**
+ *
+ * @author pariwat.nira
+ */
+public class ObjectOrientedPokemon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
