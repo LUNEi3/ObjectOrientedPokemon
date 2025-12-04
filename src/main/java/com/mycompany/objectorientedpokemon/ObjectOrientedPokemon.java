d3b5d1cbba926d061e01cfa0e56d5789feea623c
LUNEi3 <niranon.49@gmail.com>
2025-12-04 18:07:12 +0700

    Add defualt program entry point

--- src/main/java/com/mycompany/objectorientedpokemon/ObjectOrientedPokemon.java
+++ src/main/java/com/mycompany/objectorientedpokemon/ObjectOrientedPokemon.java
@@ -4,13 +4,28 @@
 
 package com.mycompany.objectorientedpokemon;
 
+import com.mycompany.objectorientedpokemon.map.GamePanel;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
 /**
  *
  * @author pariwat.nira
  */
 public class ObjectOrientedPokemon {
+    private static Dimension preferredSize = new Dimension(1280, 720);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame main = new JFrame();
+        main.setResizable(false);
+        // main.setPreferredSize(preferredSize);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.setLocationRelativeTo(null);
+        
+        GamePanel map = new GamePanel();
+        main.add(map);
+        main.pack();
+        
+        main.setVisible(true);
     }
 }
