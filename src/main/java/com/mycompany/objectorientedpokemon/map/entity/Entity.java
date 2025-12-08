7f825b4bebd99becfa5df6e77061a1c8b4cef452
LUNEi3 <niranon.49@gmail.com>
2025-12-08 16:51:57 +0700

    Add setup() to load and return BufferedImage

--- src/main/java/com/mycompany/objectorientedpokemon/map/entity/Entity.java
+++ src/main/java/com/mycompany/objectorientedpokemon/map/entity/Entity.java
@@ -4,20 +4,40 @@
  */
 package com.mycompany.objectorientedpokemon.map.entity;
 
+import com.mycompany.objectorientedpokemon.GameConstants;
+import com.mycompany.objectorientedpokemon.map.MapPanel;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author User
  */
 public class Entity {
-    public int worldX, worldY, speed;
-    
+    protected MapPanel mp;
     protected BufferedImage right1, right2, left1, left2;
+    public Rectangle solidArea = new Rectangle(0, 0, 40, 40);
     public String direction;
+    public int worldX, worldY, speed;
+    public int solidAreaDefaultX = solidArea.x, solidAreaDefaultY = solidArea.y;
     protected int spriteCounter;
     protected boolean swapSprite = false;
-    public Rectangle solidArea;
     public boolean collisionOn = false;
+    
+    public Entity(MapPanel mp) {
+        this.mp = mp;
+    }
+    
+    protected BufferedImage setup(String imagePath) {
+        BufferedImage image = null;
+         try {
+            image = ImageIO.read(getClass().getResourceAsStream(imagePath + ".png"));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+         
+         return image;
+    }
 }
