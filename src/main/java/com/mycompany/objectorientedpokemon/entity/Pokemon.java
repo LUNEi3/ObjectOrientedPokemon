38ad6134063fdaf593cc49d14b939a8ea3259dfd
LUNEi3 <niranon.49@gmail.com>
2025-12-14 21:49:43 +0700

    Update random status based on type and evolution method

--- src/main/java/com/mycompany/objectorientedpokemon/entity/Pokemon.java
+++ src/main/java/com/mycompany/objectorientedpokemon/entity/Pokemon.java
@@ -23,10 +23,12 @@ public class Pokemon {
     public ArrayList<BufferedImage> formImages;
     public ArrayList<String> names;
     
+    public int form;
     private Random rand = new Random();
     
     // Harded Fix
     public Pokemon(String name, int maxHp, int atk, int def, String type, String imagePath) {
+        this.form = 1;
         this.name = name;
         this.maxHp = maxHp;
         this.hp = maxHp;
@@ -44,6 +46,7 @@ public class Pokemon {
     }
     
     public Pokemon(String type) {
+        this.form = 1;
         this.formImages = new ArrayList<>();
         this.names = new ArrayList<>();
         this.type = type;
@@ -53,6 +56,35 @@ public class Pokemon {
 
     public Pokemon(){
         this("");
+        this.form = 1;
+    }
+    
+    public void evolution() {
+        
+        // Can't evole if reach max evolution form
+        if (this.form >= names.size()) {
+            // System.out.println("This Pokemon can't evolution anymore. " + this.form);
+            return;
+        }
+        
+        // Default evolution multiplier
+        double min = 1.3;
+        double max = 1.7;
+        
+        // In case max evolution at 2, more evolution multiplier
+        if (names.size() == 2) {
+            min = 1.8;
+            max = 2.5;
+            // System.out.println("More multiplier " + min + " " +max);
+        }
+        this.maxHp = (int) (this.maxHp * (min + (rand.nextDouble() * (max - min))));
+        this.atk = (int) (this.atk * (min + (rand.nextDouble() * (max - min))));
+        this.def = (int) (this.def * (min + (rand.nextDouble() * (max - min))));
+        form += 1;
+        
+        // Update name and image
+        this.name = this.names.get(form - 1);
+        this.image = this.formImages.get(form - 1);
     }
     
     private void randomType() {
@@ -98,13 +130,13 @@ public class Pokemon {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            } else {
-                System.out.println("Fail to load image: " + imagePath);
             }
         }
     }
     
     private void setup() {
+        
+        // Load Base Name
         String path = "/pokemon/" + this.type + "/base" + ".txt";
         ArrayList<String> arr = new ArrayList<>();
         try {
@@ -128,13 +160,8 @@ public class Pokemon {
         // Set Base Name
         this.name = arr.get(rand.nextInt(arr.size()));
         
-        if (this.name != null) {
-            loadImages();
-         } else {
-            System.out.println("Name is null!!!");
-        }
         
-        
+        // Load all names of Base Pokemon
         path = "/pokemon/" + this.type + "/" + this.name + "/name.txt";
         arr = new ArrayList<>();
         try {
@@ -154,10 +181,48 @@ public class Pokemon {
             e.printStackTrace();
         }
         
+        
         // Adding name of all Pokemon Forms to names[]
         for (String x: arr) {
             this.names.add(x);
         }
         
+        
+        // Load Images
+        if (this.name != null) {
+            loadImages();
+         } else {
+            System.out.println("Name is null!!!");
+        }
+        
+        
+        // Random Status based on Type
+        switch (this.type) {
+            case "fire" -> {
+                this.maxHp = rand.nextInt(39, 50);
+                this.atk = rand.nextInt(38, 74);
+                this.def = rand.nextInt(32, 53);
+            }
+            case "electric" -> {
+                this.maxHp = rand.nextInt(30, 55);
+                this.atk = rand.nextInt(40, 68);
+                this.def = rand.nextInt(27, 55);
+            }
+            case "leaf" -> {
+                this.maxHp = rand.nextInt(45, 60);
+                this.atk = rand.nextInt(49, 65);
+                this.def = rand.nextInt(34, 54);
+            }
+            case "rock" -> {
+                this.maxHp = rand.nextInt(42, 52);
+                this.atk = rand.nextInt(42, 60);
+                this.def = rand.nextInt(50, 80);
+            }
+            case "water" -> {
+                this.maxHp = rand.nextInt(48, 77);
+                this.atk = rand.nextInt(35, 63);
+                this.def = rand.nextInt(36, 59);
+            }
+        }
     }
 }
